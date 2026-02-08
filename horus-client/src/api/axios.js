e8130ab503065436c79f0a1314f33c66db9fc1ae
DarwinToapanta01 <datoapanta11@espe.edu.ec>
2026-02-07 22:17:07 -0500

    Eliminar archivos de PWA

--- horus-client/src/api/axios.js
+++ horus-client/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://127.0.0.1:8000/api',
     baseURL: 'http://localhost:8000/api',
-    //baseURL: 'http://192.168.0.9:8000/api',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
